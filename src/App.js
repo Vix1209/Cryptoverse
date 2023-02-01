e9246e3c0c9c91ca4df2754dc590a3d272253354
Uchenna Ofor <ucheofor2015@gmail.com>
2023-02-01 07:41:23 +0100

    'crypto-site-up'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import {Navbar, Homepage, Exchanges, Cryptocurrencies, News, CryptoDetails} from './components'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Switch> 
+                        {/* the switch allows us to have mulltiple routes  */}
+                            <Route exact path='/'>
+                            {/* this allows us to route to an exact location */}
+
+                                <Homepage />
+                                {/* this is the component you wish to rout   */}
+
+                            </Route>
+
+                            <Route exact path='/exchanges'>
+                            {/* this allows us to route to an exact location */}
+
+                                <Exchanges />
+                                {/* this is the component you wish to rout   */}
+
+                            </Route>
+
+                            <Route exact path='/cryptocurrencies'>
+                            {/* this allows us to route to an exact location */}
+
+                                <Cryptocurrencies />
+                                {/* this is the component you wish to rout   */}
+
+                            </Route>
+
+                            <Route exact path='/crypto/:coinid'>
+                            {/* this allows us to route to an exact location */}
+
+                                <CryptoDetails />
+                                {/* this is the component you wish to rout   */}
+
+                            </Route>
+
+                            <Route exact path='/news'>
+                            {/* this allows us to route to an exact location */}
+
+                                <News />
+                                {/* this is the component you wish to rout   */}
+
+                            </Route>
+                        </Switch>
+                    </div>
+
+                </Layout>
+            </div>
+            <div className='footer'>
+
+            </div>
+        </div>
+   
+    );
 }
 
 export default App;
